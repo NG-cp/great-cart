ecee487b293c698b348d2713804b962b346297f1
Nisarg Gami <nisarggami@yahoo.com>
2022-07-23 20:41:07 +0530

    Review and Rating system

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from products.models import Products, Variations
+from products.models import Products, Variations, ReviewRating, ProductGallery
 
 # Register your models here.
-
 class ProductAdmin(admin.ModelAdmin):
     list_display= ('product_name','price','stock','category','modified_date','is_available')
     prepopulated_fields = {'slug': ('product_name',)}
@@ -14,3 +13,5 @@ class VariationAdmin(admin.ModelAdmin):
 
 admin.site.register(Products, ProductAdmin)
 admin.site.register(Variations, VariationAdmin)
+admin.site.register(ReviewRating)
+admin.site.register(ProductGallery)
